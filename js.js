4e649821e11936ee4e2f673afebf7de82bad3426
AlessandroGWanguemert <126543397+AlessandroGWanguemert@users.noreply.github.com>
2023-05-15 21:29:54 +0100

    300

--- js.js
+++ js.js
@@ -15,11 +15,12 @@ window.onload = function(){
 
   });
 
-  
-  $(document).ready(function() {
+
+  /*$(document).ready(function() {
     $("#encriptar-btn").click(function() {
       let texto = $("#texto-a-encriptar").val();
       let textoEncriptado = encriptarTexto(texto);
+      
       if (textoEncriptado !== null) {
         $("#texto-a-encriptar").val(textoEncriptado);
       } else {
@@ -52,7 +53,83 @@ window.onload = function(){
       }
     }
     return textoEncriptado.trim();
+  }*/
+  $(document).ready(function() {
+    $("#encriptar-btn").click(function() {
+      let texto = $("#texto-a-encriptar").val();
+      let textoEncriptado;
+  
+      if ($("#encriptar-btn").text() === "Encrypt") {
+        textoEncriptado = encriptarTexto(texto);
+        if (textoEncriptado !== null) {
+          $("#texto-a-encriptar").val(textoEncriptado);
+        } else {
+          alert("Error: Solo se permiten letras y espacios en blanco, y el texto debe tener un máximo de 200 caracteres.");
+        }
+      } else if ($("#encriptar-btn").text() === "Decrypt") {
+        textoEncriptado = desencriptarTexto(texto);
+        if (textoEncriptado !== null) {
+          $("#texto-a-encriptar").val(textoEncriptado);
+        } else {
+          alert("Error: El texto debe tener un máximo de 200 caracteres y solo puede contener números separados por espacios.");
+        }
+      }
+    });
+  });
+  
+  function esLetra(caracter) {
+    return /^[a-zA-Z]+$/.test(caracter);
   }
+  
+  function encriptarTexto(texto) {
+    if (texto.length > 200) {
+      return null;
+    }
+  
+    let textoEncriptado = "";
+  
+    for (let i = 0; i < texto.length; i++) {
+      let caracter = texto[i];
+  
+      if (caracter !== " " && !esLetra(caracter)) {
+        return null;
+      }
+  
+      if (caracter === " ") {
+        textoEncriptado += " ";
+      } else {
+        let codigoAscii = texto.charCodeAt(i);
+        let codigoEncriptado = Math.round((codigoAscii * 60) / 4);
+        textoEncriptado += codigoEncriptado + " ";
+      }
+    }
+  
+    return textoEncriptado.trim();
+  }
+  
+  function desencriptarTexto(texto) {
+    if (texto.length > 200) {
+      return null;
+    }
+  
+    let numerosEncriptados = texto.split(" ");
+    let textoDesencriptado = "";
+  
+    for (let i = 0; i < numerosEncriptados.length; i++) {
+      let numeroEncriptado = parseInt(numerosEncriptados[i]);
+  
+      if (isNaN(numeroEncriptado)) {
+        return null;
+      }
+  
+      let codigoAscii = Math.round((numeroEncriptado * 4) / 60);
+      let caracterDesencriptado = String.fromCharCode(codigoAscii);
+      textoDesencriptado += caracterDesencriptado;
+    }
+  
+    return textoDesencriptado;
+  }
+  
   
   
   
