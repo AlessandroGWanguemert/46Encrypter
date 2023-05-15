db09186f2a0113abd762e8d59143241db75f1f8e
AlessandroGWanguemert <126543397+AlessandroGWanguemert@users.noreply.github.com>
2023-05-15 19:30:48 +0100

    200

--- js.js
+++ js.js
@@ -1,11 +1,6 @@
 
 window.onload = function () {
-    const modoOscuroBtn = document.getElementById("modo-oscuro");
-const body = document.body;
+    
 
-modoOscuroBtn.addEventListener("click", () => {
-  body.classList.toggle("dark");
- 
 
-});
 }
