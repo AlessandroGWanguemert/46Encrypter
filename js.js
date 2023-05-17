6a0ef959d13ee33f2422cd6fe45fb235d5a7c74b
AlessandroGWanguemert <126543397+AlessandroGWanguemert@users.noreply.github.com>
2023-05-17 16:43:24 +0100

    310

--- js.js
+++ js.js
@@ -15,45 +15,6 @@ window.onload = function(){
 
   });
 
-
-  /*$(document).ready(function() {
-    $("#encriptar-btn").click(function() {
-      let texto = $("#texto-a-encriptar").val();
-      let textoEncriptado = encriptarTexto(texto);
-      
-      if (textoEncriptado !== null) {
-        $("#texto-a-encriptar").val(textoEncriptado);
-      } else {
-        alert("Error: Solo se permiten letras y espacios en blanco, y el texto debe tener un máximo de 200 caracteres.");
-      }
-    });
-  });
-  
-  
-  function esLetra(caracter) {
-    return /^[a-zA-Z]+$/.test(caracter);
-  }
-  
-  function encriptarTexto(texto) {
-    if (texto.length > 200) {
-      return null;
-    }
-    let textoEncriptado = "";
-    for (let i = 0; i < texto.length; i++) {
-      let caracter = texto[i];
-      if (caracter !== " " && !esLetra(caracter)) {
-        return null;
-      }
-      if (caracter === " ") {
-        textoEncriptado += " ";
-      } else {
-        let codigoAscii = texto.charCodeAt(i);
-        let codigoEncriptado = Math.round((codigoAscii * 60) / 4);
-        textoEncriptado += codigoEncriptado + " ";
-      }
-    }
-    return textoEncriptado.trim();
-  }*/
   $(document).ready(function() {
     $("#encriptar-btn").click(function() {
       let texto = $("#texto-a-encriptar").val();
