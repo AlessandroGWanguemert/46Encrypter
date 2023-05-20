fb78c3a6775b461fd668d1fbe15e860b40077833
AlessandroGWanguemert <126543397+AlessandroGWanguemert@users.noreply.github.com>
2023-05-20 18:35:38 +0100

    prueba 00

--- js.js
+++ js.js
@@ -7,11 +7,9 @@ window.onload = function() {
       $(".h16").addClass("show");
       $(".Encrypth1").addClass("show");
     }, 200);
-    
   });
 
-
-/*----------------------------Switch-btn------------------------------- */
+  /*----------------------------Switch-btn------------------------------- */
   $(document).ready(function() {
     $(".Decrypt").on("click", function() {
       $("#encriptar-btn").text("Decrypt");
@@ -21,7 +19,8 @@ window.onload = function() {
       $("#encriptar-btn").text("Encrypt");
     });
   });
-/*----------------------------Encrypt-Decrypt-----------------------------*/
+
+  /*----------------------------Encrypt-Decrypt-----------------------------*/
   $(document).ready(function() {
     $("#encriptar-btn").click(function() {
       let texto = $("#texto-a-encriptar").val();
@@ -68,81 +67,48 @@ window.onload = function() {
       } else {
         let codigoAscii = texto.charCodeAt(i);
         let codigoEncriptado = Math.round((codigoAscii * 60) / 4);
-        textoEncriptado += codigoEncriptado + " ";
+        textoEncriptado += codigoEncriptado + "00";
       }
     }
 
     return textoEncriptado.trim();
   }
 
-  function encriptarTexto(texto) {
-    if (texto.length > 200) {
-      return null;
-    }
-  
-    let palabras = texto.trim().split(" ");
-    let palabrasEncriptadas = [];
-  
-    for (let i = 0; i < palabras.length; i++) {
-      let palabra = palabras[i];
-      let palabraEncriptada = "";
-  
-      for (let j = 0; j < palabra.length; j++) {
-        let caracter = palabra[j];
-  
-        if (!esLetra(caracter)) {
-          return null;
-        }
-  
-        let codigoAscii = caracter.charCodeAt(0);
-        let codigoEncriptado = Math.round((codigoAscii * 60) / 4);
-        palabraEncriptada += codigoEncriptado + ",";
-      }
-  
-      palabrasEncriptadas.push(palabraEncriptada.slice(0, -1));
-    }
-  
-    let textoEncriptado = palabrasEncriptadas.join(" ");
-  
-    return textoEncriptado;
-  }
-  
   function desencriptarTexto(texto) {
     if (texto.length > 200) {
       return null;
     }
-  
+
     let palabrasEncriptadas = texto.trim().split(" ");
     let palabrasDesencriptadas = [];
-  
+
     for (let i = 0; i < palabrasEncriptadas.length; i++) {
       let palabraEncriptada = palabrasEncriptadas[i];
-  
+
       if (palabraEncriptada === "") {
         continue;
       }
-  
-      let numerosEncriptados = palabraEncriptada.split(",");
+
+      let numerosEncriptados = palabraEncriptada.split("00");
       let palabraDesencriptada = "";
-  
+
       for (let j = 0; j < numerosEncriptados.length; j++) {
         let numeroEncriptado = parseInt(numerosEncriptados[j]);
-  
+
         if (isNaN(numeroEncriptado)) {
           return null;
         }
-  
+
         let codigoAscii = Math.round((numeroEncriptado * 4) / 60);
         let caracterDesencriptado = String.fromCharCode(codigoAscii);
         palabraDesencriptada += caracterDesencriptado;
       }
-  
+
       palabrasDesencriptadas.push(palabraDesencriptada);
     }
-  
+
     let textoDesencriptado = palabrasDesencriptadas.join(" ");
-  
+
     return textoDesencriptado;
   }
-  
 }
